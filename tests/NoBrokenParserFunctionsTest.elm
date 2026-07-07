8dcc3b59ce98ed7e441593e7704c2b071ca4d119
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-07-08 00:40:22 +0200

    Check Parser.int

--- tests/NoBrokenParserFunctionsTest.elm
+++ tests/NoBrokenParserFunctionsTest.elm
@@ -42,20 +42,23 @@ a = lineComment
             \() ->
                 """module A exposing (..)
 
-import Parser.Advanced exposing (lineComment)
+import Parser.Advanced as Fuzzy
 
-a = lineComment
+a = Fuzzy.int
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectErrors
                         [ Review.Test.error
-                            { message = "Do not use Parser.Advanced.lineComment"
-                            , details = [ "That function desyncs the parser's internal state. Use one of the functions from pithub/elm-parser-bug-workaround instead." ]
-                            , under = "lineComment"
+                            { message = "Do not use Parser.Advanced.int"
+                            , details =
+                                [ "That function is based on one which parsing floats, which means that it has surprising behaviors around `e` and `.`."
+                                , "Use `Fuzzy.chompWhile Char.isDigit` instead."
+                                ]
+                            , under = "Fuzzy.int"
                             }
                             |> Review.Test.atExactly
                                 { start = { row = 5, column = 5 }
-                                , end = { row = 5, column = 16 }
+                                , end = { row = 5, column = 14 }
                                 }
                         ]
         ]
